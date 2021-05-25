9473976eec254531451b0d2c98b91273d6888f7e
mazoyafashions <80321054+mazoyafashions@users.noreply.github.com>
2021-05-25 18:36:08 +0530

    Add top bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+const routes: Routes = [
+  {
+    path: 'home',
+    component:HomeComponent
 
-const routes: Routes = [];
+  },
+  // {
+  //   path: 'about',
+  //   component:AboutComponent
+
+  // },
+  // {
+  //   path: 'about',
+  //   component:AboutComponent
+
+  // },
+  // {
+  //   path: 'product',
+  //   // loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+  // },
+  // {
+  //   path: 'add-to-cart',
+  //   // component:AddToCartComponent
+
+  // },
+  // {
+  //   path: 'google-map',
+  //   // component:GoogleMapComponent
+
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
