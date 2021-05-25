71aeee893a7e4a7225dd512712ea73e687779391
mazoyafashions <80321054+mazoyafashions@users.noreply.github.com>
2021-05-25 19:24:38 +0530

    side bar hide

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import {MatDialog} from '@angular/material/dialog';
+import {MatDialog} from '@angular/material/dialog';
 // import { ModalContentComponent } from '../modal-content/modal-content.component';
 
 
