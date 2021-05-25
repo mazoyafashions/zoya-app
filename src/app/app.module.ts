da446665c1f860de6336ceac9e8fee53f03eced9
mazoyafashions <80321054+mazoyafashions@users.noreply.github.com>
2021-05-25 20:00:30 +0530

    add side

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
 
 @NgModule({
   declarations: [
@@ -46,6 +48,7 @@ import {MatPaginatorModule} from '@angular/material/paginator';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     BrowserModule,
     MatInputModule,
