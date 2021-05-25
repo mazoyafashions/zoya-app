6c0a826c4355f32c1bb9e9d7bbba8fe13eafd341
mazoyafashions <80321054+mazoyafashions@users.noreply.github.com>
2021-05-25 22:03:09 +0530

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,17 @@ import {AboutComponent} from './about/about.component';
 import { AddToCartComponent } from './products/add-to-cart/add-to-cart.component';
 const routes: Routes = [
   {
+    path: '',
+    redirectTo:'home',
+    pathMatch:'full'
+
+  },
+  {
     path: 'home',
     component:HomeComponent
 
   },
-  {
-    path: '',
-    component:HomeComponent
-
-  },
+  
   {
     path: 'about',
     component:AboutComponent
